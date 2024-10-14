23dedcd5461ee1f3b8d40aa2929a8a1e113ebc0f
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2024-10-14 16:59:53 -0300

    fea: creating common domain and order service domain

--- food-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/BaseId.java
+++ food-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/BaseId.java
@@ -1,12 +1,28 @@
 package com.food.ordering.system.domain.valueobject;
 
-import lombok.Data;
+import java.util.Objects;
 
-@Data
 public abstract class BaseId<T> {
     private final T value;
 
     protected BaseId(T value){
         this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseId<?> baseId = (BaseId<?>) o;
+        return Objects.equals(value, baseId.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
     }
 }
--- food-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
+++ food-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
@@ -1,16 +1,31 @@
 package com.food.ordering.system.domain.valueobject;
 
-import lombok.Data;
-
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Objects;
 
-@Data
 public class Money {
     private final BigDecimal amount;
 
     public Money(BigDecimal amount) {
         this.amount = amount;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return Objects.equals(amount, money.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(amount);
     }
 
     public boolean isGreaterThanZero() {
