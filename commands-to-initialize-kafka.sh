0394c6c41cbaa958dae2a13d67a5e95050f11384
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2025-02-17 19:36:43 -0300

    fix: adding commands to start bdd

--- commands-to-initialize-kafka.sh
+++ commands-to-initialize-kafka.sh
@@ -9,3 +9,40 @@ Commands to initialize kafka:
 (open in new terminal fron intellij-module-infrastructure) docker-compose -f common.yml -f init_kafka.yml up
 
 Una vez utilizado los docker terminarlos con ctrl + c y para el caso de colima (Desde un terminal) colima stop
+
+Commands to initialize bdd:
+
+- Verifique si ha sido iniciado antes
+
+brew services list
+
+Name          Status  User             File
+colima        none
+postgresql@14 started marcelocartagena ~/Library/LaunchAgents/homebrew.mxcl.postgresql@14.plist
+postgresql@16 none
+unbound       none
+
+marcelocartagena@MacBook-Pro-de-mcartagena ~ $ psql postgres
+
+psql (14.15 (Homebrew))
+Type "help" for help.
+
+postgres=# CREATE ROLE postgres WITH LOGIN SUPERUSER PASSWORD 'postgres';
+CREATE ROLE
+postgres=# \du
+                                       List of roles
+    Role name     |                         Attributes                         | Member of
+------------------+------------------------------------------------------------+-----------
+ marcelocartagena | Superuser, Create role, Create DB, Replication, Bypass RLS | {}
+ postgres         | Superuser                                                  | {}
+
+postgres=# exit
+
+- Si el servicio no ha levantado
+
+brew services start postgresql
+
+En caso de no funcionar probar:
+
+- to start postgressql in local terminal: pg_ctl -D /usr/local/var/postgres -l logfile start
+
