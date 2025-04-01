ad267f44937f9c4ba5e818c120ed84b3edc41fe6
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2025-04-01 12:43:52 -0300

    fea: deploying confluent kafka to local kubernetes usinng cp-helm-charts

--- commands-to-initialize-kafka.sh
+++ commands-to-initialize-kafka.sh
@@ -46,3 +46,41 @@ En caso de no funcionar probar:
 
 - to start postgressql in local terminal: pg_ctl -D /usr/local/var/postgres -l logfile start
 
+----------------------------------------------------------
+
+Commands to install and use minikube:
+
+brew install minikube
+
+brew install kubectl
+
+brew install helm
+
+colima start --cpu 4 --memory 10 --kubernetes
+
+minikube start --cpus 4 --memory 8192 --driver=docker
+
+minikube status
+
+helm install local-confluent-kafka ./helm/cp-helm-charts --version 0.6.0
+
+kubectl get pods -A
+(The -A flag shows all namespaces.)
+
+kubectl apply -f kafka-client.yml
+
+kubectl exec -it kafka-client -- /bin/bash
+
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic customer --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic payment-request --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic payment-response --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic restaurant-approval-request --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic restaurant-approval-response --create --partitions 3 --replication-factor 3 --if-not-exists
+
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --list
+
+helm uninstall local-confluent-kafka
+
+kubectl delete -f kafka-client.yml
+
+
